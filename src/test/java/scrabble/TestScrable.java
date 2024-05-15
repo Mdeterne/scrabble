6a13c5ffc0a43c49c3cc7bdd9a466aa462085218
Mateo Deterne <mateodeterne@gmail.com>
2024-05-15 10:27:49 +0200

    creation méthode estVide dan plateau et test cration de plateau vide

---
+++ src/test/java/scrabble/TestScrable.java
@@ -0,0 +1,31 @@
+package scrabble;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import scrabble.modele.Chevalet;
+import scrabble.modele.Plateau;
+
+class TestScrable {
+	
+	@Test
+	void nouveauPlateauDoitEtreVide() {
+		Plateau plateau = new Plateau();
+		assertTrue(plateau.estVide());
+	}
+	
+	@Test
+	void remplirMonPlateauDoitMettreDesCases() {
+		Plateau plateau = new Plateau();
+		plateau.mettreDesCaseDansMonPlateau();
+		assertFalse(plateau.estVide());
+	}
+	
+	@Test
+	void nouveauChevaletDoitEtreVide() {
+		Chevalet chevalet = new Chevalet();
+		assertTrue(chevalet.estVide());
+	}
+}
