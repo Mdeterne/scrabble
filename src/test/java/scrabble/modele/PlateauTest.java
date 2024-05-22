d5c7dabfc102d71208dede0af4f474d73e7bdc6d
Julien Coulaud <julien.coulaud@etu.unilim.fr>
2024-05-22 15:06:45 +0200

    plateauTest fini

--- src/test/java/scrabble/modele/PlateauTest.java
+++ src/test/java/scrabble/modele/PlateauTest.java
@@ -1,21 +1,17 @@
 package scrabble.modele;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
 class PlateauTest {
 	
 	@Test
-	void nouveauPlateauDoitEtreVide() {
+	void doitAjouterUnJeton() {
 		Plateau plateau = new Plateau();
-		assertTrue(plateau.estVide());
-	}
-	
-	@Test
-	void remplirMonPlateauDoitMettreDesCases() {
-		Plateau plateau = new Plateau();
-		assertFalse(plateau.estVide());
+		Jeton jeton = new Jeton(Lettre.J, Points.QUATRE);
+		Position position = new Position(1, 1);
+		plateau.ajouterJeton(jeton, position);
+		assertEquals(Lettre.J, plateau.cases[0][0].getJeton().getLettre());
 	}
 }
