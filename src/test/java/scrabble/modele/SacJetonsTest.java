bd5cf93e0b713848ca3946203ebbdeec6cce72f5
Julien Coulaud <julien.coulaud@etu.unilim.fr>
2024-05-24 16:54:25 +0200

    tests SacJetons finis

--- src/test/java/scrabble/modele/SacJetonsTest.java
+++ src/test/java/scrabble/modele/SacJetonsTest.java
@@ -3,6 +3,9 @@ package scrabble.modele;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 import scrabble.gui.Console;
@@ -20,11 +23,10 @@ class SacJetonsTest {
 	void piocherUnJeton() {
 		SacJeton sacJeton = new SacJeton();
 		sacJeton.mettreDesJetonDansMonSac();
-		Jeton jeton = null;
 		int nbJetonsApresPioche = sacJeton.nbJeton()-1;
 		
 		try {
-			jeton = sacJeton.piocherJeton();
+			sacJeton.piocherJeton();
 		} catch (SacVideException e) {
 			Console.message("le sac de jeton est vide");
 		}
@@ -34,16 +36,35 @@ class SacJetonsTest {
 	@Test
 	void piocherAlorsQueLeSacEstVide() {
 		SacJeton sacJeton = new SacJeton();
-		Jeton jeton = null;
 		boolean exception = false;
 		
 		try {
-			jeton = sacJeton.piocherJeton();
+			sacJeton.piocherJeton();
 		} catch (SacVideException e) {
 			Console.message("le sac de jeton est vide");
 			exception = true;
 		}
 		
 		assertTrue(exception);
+	}
+	
+	@Test
+	void doitAjouterUnJeton() {
+		SacJeton sacJeton = new SacJeton();
+		Jeton jeton = new Jeton(Lettre.N, Points.UN);
+		sacJeton.ajouterUnJeton(jeton);
+		assertEquals(1, sacJeton.jetons.size());
+	}
+	
+	@Test
+	void doitAjouterUneListeDeJetons() {
+		SacJeton sacJeton = new SacJeton();
+		Jeton jeton1 = new Jeton(Lettre.N, Points.UN);
+		Jeton jeton2 = new Jeton(Lettre.O, Points.UN);
+		List<Jeton> jetons = new ArrayList<Jeton>();
+		jetons.add(jeton1);
+		jetons.add(jeton2);
+		sacJeton.ajouterUneListeJeton(jetons);
+		assertEquals(2, sacJeton.jetons.size());
 	}
 }
