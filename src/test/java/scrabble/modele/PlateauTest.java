794ea41e2ec288c43a0d8e5ed7fc61f222c4739f
Julien Coulaud <julien.coulaud@etu.unilim.fr>
2024-05-27 11:39:31 +0200

    correction PlateauTest

--- src/test/java/scrabble/modele/PlateauTest.java
+++ src/test/java/scrabble/modele/PlateauTest.java
@@ -8,10 +8,10 @@ class PlateauTest {
 	
 	@Test
 	void doitAjouterUnJeton() {
-		Plateau plateau = new Plateau();
-		Jeton jeton = new Jeton(Lettre.J, Points.QUATRE);
-		Position position = new Position(1, 1);
-		plateau.ajouterJeton(jeton, position);
-		assertEquals(Lettre.J, plateau.cases[0][0].getJeton().getLettre());
+	    Plateau plateau = new Plateau();
+	    Jeton jeton = new Jeton(Lettre.J, Points.QUATRE);
+	    Position position = new Position(1, 1);
+	    plateau.ajouterJeton(jeton, position);
+	    assertEquals(Lettre.J.getLettre(), plateau.cases[0][0].getJeton().getLettre());
 	}
 }
